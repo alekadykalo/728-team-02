d946cca916e5e1974222be6a5543d7dc69f58cd0
alekadykalo <alessandrokadykalo17@gmail.com>
2023-01-02 18:15:30 +0200

    added @media to  testimonials.css + modified testimonials.js

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -12,4 +12,31 @@ var swiper = new Swiper(".swiper", {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
+	breakpoints: {
+		1024: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+			slidesPerGroup: 1,
+		},
+		765: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+			slidesPerGroup: 1,
+		},
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			slidesPerGroup: 1,
+		},
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			slidesPerGroup: 1,
+		},
+		0: {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			slidesPerGroup: 1,
+		}
+	}
 });
