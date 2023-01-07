89bc419d1ad2a44c532dcf650fc6b5011d78fbb3
alekadykalo <alessandrokadykalo17@gmail.com>
2023-01-07 12:17:17 +0200

    modified testimonials section + testimonials.css + testimonials.js | added mobile first

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".swiper", {
-	slidesPerView: 2,
+	slidesPerView: 1,
 	spaceBetween: 30,
 	slidesPerGroup: 1,
 	loop: false,
@@ -13,28 +13,8 @@ var swiper = new Swiper(".swiper", {
 		prevEl: ".swiper-button-prev",
 	},
 	breakpoints: {
-		1024: {
+		960: {
 			slidesPerView: 2,
-			spaceBetween: 30,
-			slidesPerGroup: 1,
-		},
-		765: {
-			slidesPerView: 2,
-			spaceBetween: 30,
-			slidesPerGroup: 1,
-		},
-		640: {
-			slidesPerView: 1,
-			spaceBetween: 30,
-			slidesPerGroup: 1,
-		},
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 30,
-			slidesPerGroup: 1,
-		},
-		0: {
-			slidesPerView: 1,
 			spaceBetween: 30,
 			slidesPerGroup: 1,
 		}
