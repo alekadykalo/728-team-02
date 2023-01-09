da1d19bcdfed055bba028c471926025ba25b8829
iryna-ivanova <119043008+iryna-ivanova@users.noreply.github.com>
2023-01-09 19:38:12 +0000

    partners js

--- js/partners-team-carousel.js
+++ js/partners-team-carousel.js
@@ -1,23 +1,26 @@
 window.addEventListener('load', () => {
-	const leftBtn = document.querySelector('.carousel__prev');
-	const rightBtn = document.querySelector('.carousel__next');
-	const sld = document.querySelector('.partners__carousel');
-	const img = document.querySelectorAll('.partners__carousel-img');
-	let counter = 0;
-	const stepSize = img[0].clientWidth;
+    const leftBtn = document.querySelector('.carousel__prev');
+    const rightBtn = document.querySelector('.carousel__next');
+    const sld = document.querySelector('.partners__carousel');
+    const img = document.querySelectorAll('.partners__carousel-img');
+    let counter = 0;
+    const stepSize = img[0].clientWidth;
 
-	sld.style.transform = 'translateX(' + `${stepSize * counter}px)`;
-	rightBtn.addEventListener('click', () => {
-		if (counter >= img.length -1) counter = - 1;
-		counter++;
-		sld.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
-	});
+    sld.style.transform = 'translateX(' + `${stepSize * counter}px)`;
+    rightBtn.addEventListener('click', () => {
+        if (window.innerWidth < 640 && counter >= img.length - 1) counter = - 1;
+        else if (window.innerWidth >= 640 && counter > 5) counter = -1;
+        else if (window.innerWidth >= 950 && counter > 4) counter = -1;
+        counter++;
+        sld.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
+    });
 
-	leftBtn.addEventListener('click', () => {
-		if (counter <= 0) counter = img.length;
-		counter--;
-		sld.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
-	})
+    leftBtn.addEventListener('click', () => {
+        if (window.innerWidth < 640 && counter <= 0) counter = img.length;
+        else if (window.innerWidth >= 640 && counter <= 0) counter = img.length - 1;
+        counter--;
+        sld.style.transform = 'translateX(' + `${-stepSize * counter}px)`;
+    });
+
+
 })
-
-
